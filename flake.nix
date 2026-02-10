cda485e6e70dc3db8c46ca4f52901ca470db3e28
mgiannopoulos24 <79588074+mgiannopoulos24@users.noreply.github.com>
2026-02-10 19:45:53 +0200

    Nixify the project

---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+  description = "HTB Progress Tracker environment";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+  };
+
+  outputs = { self, nixpkgs }: let
+    system = "x86_64-linux";
+    pkgs = import nixpkgs {
+      inherit system;
+    };
+    pythonEnv = pkgs.python3.withPackages (ps: with ps; [
+      requests
+      discordpy
+      python-dotenv
+      tzdata
+      black
+    ]);
+  in {
+    devShells.${system}.default = pkgs.mkShell {
+      packages = [
+        bash
+        pythonEnv
+      ];
+    };
+  };
+}
